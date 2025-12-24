8a928aa9629754bc0932f0d496bf7fecf6f523cd
PawelBorowiecki <pawelbor77@gmail.com>
2025-12-24 16:14:00 +0100

    Init

---
+++ src/main/java/com/pz/salonserwis2/SalonSerwis2Application.java
@@ -0,0 +1,13 @@
+package com.pz.salonserwis2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonSerwis2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalonSerwis2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pz/salonserwis2/SalonSerwis2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pz.salonserwis2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonSerwis2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
