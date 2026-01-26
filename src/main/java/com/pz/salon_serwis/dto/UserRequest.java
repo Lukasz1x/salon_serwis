47990b335386220483f381b226614b2ff30d59cf
PawelBorowiecki <pawelbor77@gmail.com>
2026-01-26 18:29:51 +0100

    create: LoginRequest, LoginResponse, UserRequest

---
+++ src/main/java/com/pz/salon_serwis/dto/UserRequest.java
@@ -0,0 +1,57 @@
+package com.pz.salon_serwis.dto;
+
+public class UserRequest {
+    private String lastName;
+    private String firstName;
+    private String phone;
+    private String email;
+    private String password;
+
+    public UserRequest(String lastName, String firstName, String phone, String email, String password) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.phone = phone;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
