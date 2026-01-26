42fca2d19819c42c2566309c0176ea3c75e9834f
PawelBorowiecki <pawelbor77@gmail.com>
2026-01-26 18:33:29 +0100

    add: findByEmail method in UserRepository

--- src/main/java/com/pz/salon_serwis/repository/UserRepository.java
+++ src/main/java/com/pz/salon_serwis/repository/UserRepository.java
@@ -2,12 +2,13 @@ package com.pz.salon_serwis.repository;
 
 import com.pz.salon_serwis.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-
-    Optional<User> findById(int id);
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    Optional<User> findByEmail(String email);
     void deleteById(int id);
 }
