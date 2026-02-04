011aaaa9cf17c94300a58e479addeda92fe1a1b9
PawelBorowiecki <pawelbor77@gmail.com>
2026-02-04 12:52:38 +0100

    Add fields to LocationRequest

--- src/main/java/com/pz/salon_serwis/dto/LocationRequest.java
+++ src/main/java/com/pz/salon_serwis/dto/LocationRequest.java
@@ -1,24 +1,68 @@
 package com.pz.salon_serwis.dto;
 
+import com.pz.salon_serwis.model.LocationType;
+
 import java.math.BigDecimal;
 
 public class LocationRequest {
-    private int locationId;
-    private BigDecimal longitude;
+    private String name;
+    private String phone;
+    private String street;
+    private String city;
+    private String zipCode;
     private BigDecimal latitude;
+    private BigDecimal longitude;
+    private LocationType locationType;
 
-    public LocationRequest(int locationId, BigDecimal longitude, BigDecimal latitude) {
-        this.locationId = locationId;
-        this.longitude = longitude;
+    public LocationRequest(String name, String phone, String street, String city, String zipCode, BigDecimal latitude, BigDecimal longitude, LocationType locationType) {
+        this.name = name;
+        this.phone = phone;
+        this.street = street;
+        this.city = city;
+        this.zipCode = zipCode;
         this.latitude = latitude;
+        this.longitude = longitude;
+        this.locationType = locationType;
     }
 
-    public int getLocationId() {
-        return locationId;
+    public String getName() {
+        return name;
     }
 
-    public void setLocationId(int locationId) {
-        this.locationId = locationId;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
     }
 
     public BigDecimal getLongitude() {
@@ -35,5 +79,13 @@ public class LocationRequest {
 
     public void setLatitude(BigDecimal latitude) {
         this.latitude = latitude;
+    }
+
+    public LocationType getLocationType() {
+        return locationType;
+    }
+
+    public void setLocationType(LocationType locationType) {
+        this.locationType = locationType;
     }
 }
