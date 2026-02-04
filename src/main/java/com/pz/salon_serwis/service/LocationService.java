3cd36ccdf239e5bcba0470e4973949548da5dc39
PawelBorowiecki <pawelbor77@gmail.com>
2026-02-04 12:54:15 +0100

    Create addLocation method in LocationService

--- src/main/java/com/pz/salon_serwis/service/LocationService.java
+++ src/main/java/com/pz/salon_serwis/service/LocationService.java
@@ -1,10 +1,12 @@
 package com.pz.salon_serwis.service;
 
 import com.pz.salon_serwis.model.Location;
+import com.pz.salon_serwis.model.LocationType;
 
 import java.math.BigDecimal;
 
 public interface LocationService {
     Location findById(int locationId);
     Location findByLongLat(BigDecimal longitude, BigDecimal latitude);
+    Location addLocation(String name, String phone, String street, String city, String zipCode, BigDecimal latitude, BigDecimal longitude, LocationType locationType);
 }
--- src/main/java/com/pz/salon_serwis/service/impl/LocationServiceImpl.java
+++ src/main/java/com/pz/salon_serwis/service/impl/LocationServiceImpl.java
@@ -1,6 +1,7 @@
 package com.pz.salon_serwis.service.impl;
 
 import com.pz.salon_serwis.model.Location;
+import com.pz.salon_serwis.model.LocationType;
 import com.pz.salon_serwis.repository.LocationRepository;
 import com.pz.salon_serwis.service.LocationService;
 import org.springframework.stereotype.Service;
@@ -23,5 +24,11 @@ public class LocationServiceImpl implements LocationService {
     @Override
     public Location findByLongLat(BigDecimal longitude, BigDecimal latitude) {
         return locationRepository.findByLongitudeAndLatitude(longitude,latitude).orElse(null);
+    }
+
+    @Override
+    public Location addLocation(String name, String phone, String street, String city, String zipCode, BigDecimal latitude, BigDecimal longitude, LocationType locationType) {
+        Location location = new Location(name, phone, street, city, zipCode, latitude, longitude, locationType);
+        return locationRepository.save(location);
     }
 }
