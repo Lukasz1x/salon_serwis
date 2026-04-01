0010b057c11dc1f9814b511fea10b950e064794e
PawelBorowiecki <pawelbor77@gmail.com>
2026-04-01 14:57:30 +0200

    Add getAll method in RepairOrderService

--- src/main/java/com/pz/salon_serwis/service/RepairOrderService.java
+++ src/main/java/com/pz/salon_serwis/service/RepairOrderService.java
@@ -1,13 +1,14 @@
 package com.pz.salon_serwis.service;
 
 import com.pz.salon_serwis.model.RepairOrder;
-import org.springframework.web.bind.annotation.PathVariable;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Map;
 
 public interface RepairOrderService {
+    List<RepairOrder> getAll();
     RepairOrder generateRepairOrder(int appointmentId, int mechanicId);
     RepairOrder addWorkDescription(int repairOrderId, Map<String, BigDecimal> description);
     RepairOrder addFinalDate(int repairOrderId, LocalDateTime date);
