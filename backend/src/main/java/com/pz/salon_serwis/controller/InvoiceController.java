fd235c2c1ec64c37b87312d103a0b7d44441f5ac
mkkrzysztof <krzysztof0mielnik@gmail.com>
2026-05-22 19:05:48 +0200

    Backend: added 2 GET endpoints for Invoices

--- backend/src/main/java/com/pz/salon_serwis/controller/InvoiceController.java
+++ backend/src/main/java/com/pz/salon_serwis/controller/InvoiceController.java
@@ -5,8 +5,11 @@ import com.pz.salon_serwis.dto.SaleInvoiceRequest;
 import com.pz.salon_serwis.model.Invoice;
 import com.pz.salon_serwis.model.User;
 import com.pz.salon_serwis.service.InvoiceService;
+import com.pz.salon_serwis.service.PdfGeneratorService;
 import com.pz.salon_serwis.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,11 +22,13 @@ import java.util.Optional;
 public class InvoiceController {
     private final InvoiceService invoiceService;
     private final UserService userService;
+    private final PdfGeneratorService pdfGeneratorService;
 
     @Autowired
-    public InvoiceController(InvoiceService invoiceService, UserService userService) {
+    public InvoiceController(InvoiceService invoiceService, UserService userService, PdfGeneratorService pdfGeneratorService) {
         this.invoiceService = invoiceService;
         this.userService = userService;
+        this.pdfGeneratorService = pdfGeneratorService;
     }
 
     @PostMapping("/generateSale")
@@ -66,4 +71,43 @@ public class InvoiceController {
         }
     }
 
+    @GetMapping("/getSaleInvoice")
+    public ResponseEntity<byte[]> getSaleInvoice(@RequestParam String invoiceId){
+        try{
+            Optional<Invoice> invoice = invoiceService.getInvoiceById(invoiceId);
+            if (invoice.isPresent()){
+                byte[] pdfBytes = pdfGeneratorService.generateInvoice(invoice.get(), PdfGeneratorService.TYPE.SALES_ORDER);
+                String filename = "SaleInvoice-" + invoice.get().getDueDate().toString() + ".pdf";
+
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION,
+                                "inline; filename=\"" + filename + "\"")
+                        .contentType(MediaType.APPLICATION_PDF)
+                        .body(pdfBytes);
+            }
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(("Error: " + e.getMessage()).getBytes());
+        }
+        return ResponseEntity.badRequest().body(("Error: Invoice not found").getBytes());
+    }
+
+    @GetMapping("/getRepairInvoice")
+    public ResponseEntity<byte[]> getRepairInvoice(@RequestParam String invoiceId){
+        try{
+            Optional<Invoice> invoice = invoiceService.getInvoiceById(invoiceId);
+            if(invoice.isPresent()){
+                byte[] pdfBytes = pdfGeneratorService.generateInvoice(invoice.get(), PdfGeneratorService.TYPE.SERVICE_ORDER);
+                String filename = "RepairInvoice-" + invoice.get().getDueDate().toString() + ".pdf";
+
+                return ResponseEntity.ok()
+                        .header(HttpHeaders.CONTENT_DISPOSITION,
+                                "inline; filename=\"" + filename + "\"")
+                        .contentType(MediaType.APPLICATION_PDF)
+                        .body(pdfBytes);
+            }
+        } catch (Exception e){
+            return ResponseEntity.internalServerError().body(("Error: " + e.getMessage()).getBytes());
+        }
+            return ResponseEntity.badRequest().body(("Error: Invoice not found").getBytes());
+    }
 }
